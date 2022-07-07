c976566575e2b6da8edde1b3e498a198d086d814
Pablo Alan <pabloxt14@gmail.com>
2022-07-07 18:18:27 -0300

    feat: implemented useRepository

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,55 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const newUser = new User();
+
+    Object.assign(newUser, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(newUser);
+
+    return newUser;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const userExists = this.users.find((user) => user.id === id);
+
+    if (!userExists) return undefined;
+
+    return userExists;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const userExists = this.users.find((user) => user.email === email);
+
+    if (!userExists) return undefined;
+
+    return userExists;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const userUpdated = Object.assign(receivedUser, {
+      admin: true,
+      updated_at: new Date(),
+    });
+
+    // Inserindo userUpdated no banco
+    this.users = this.users.map((user) => {
+      if (user.id === userUpdated.id) {
+        Object.assign(user, userUpdated);
+      }
+      return user;
+    });
+
+    return userUpdated;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
